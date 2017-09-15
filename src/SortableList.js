570d6f7d33b853ec4b8b31166548ad65472d07c0
Hawken Rives <hawkrives@gmail.com>
2017-09-14 20:46:35 -0500

    Switch to prop-types in SortableList

--- src/SortableList.js
+++ src/SortableList.js
@@ -1,4 +1,5 @@
-import React, {Component, PropTypes} from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import {Animated, ScrollView, View, Platform, StyleSheet} from 'react-native';
 import {shallowEqual, swapArrayElements} from './utils';
 import Row from './Row';
